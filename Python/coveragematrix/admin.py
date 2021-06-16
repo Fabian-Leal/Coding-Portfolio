907dbf995fdaaaba637da10834ed776d54a32ae1
Fabtex <fabianlealv@gmail.com>
2021-06-16 00:23:30 -0400

    Delete Python/coveragematrix directory

--- Python/coveragematrix/admin.py
+++
@@ -1,5 +0,0 @@
-from coveragematrix.models import CoverageMatrixPlan, CoverageMatrixTask
-from django.contrib import admin
-
-admin.site.register(CoverageMatrixPlan)
-admin.site.register(CoverageMatrixTask)
